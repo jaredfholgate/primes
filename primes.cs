49d2fd544cdf4a3095134d96b4b5100354353f81
Jared Holgate <jaredfholgate@gmail.com>
2021-08-30 23:03:08 +0100

    More optimisations

--- primes.cs
+++ primes.cs
@@ -6,30 +6,56 @@ namespace primes
 {
     public class PrimeGenerator
     {
-        public void Run()
+        private void IncrementChecks(List<long> checks, long maxCheck)
         {
-            var primes = new List<long> {1,2,5};
-            var current = 5;
-            var validPrimes = new List<char> { '1', '3', '7', '9'};
-            while(true)
+            for(long i = checks.Any() ? checks.Max() : 3; i <= maxCheck; i+=2)
             {
-                current++;
-                if(validPrimes.Contains(current.ToString().Last()))
+                if(validPrimes.Contains(i.ToString().Last()))
                 {
-                    var isPrime = true;
-                    for(long i = 3; i < current; i+=2)
+                    var validCheck = true;
+                    foreach(var check in checks)
                     {
-                        if(current % i == 0)
+                        if(i % check == 0)
                         {
-                            isPrime = false;
+                            validCheck = false;
                             break;
                         }
                     }
-
-                    if(isPrime)
+                    if(validCheck)
                     {
-                        primes.Add(current);
-                        Console.WriteLine(current);
+                        checks.Add(i);
+                    }
+                }
+            }
+        }
+
+        private List<char> validPrimes = new List<char> { '1', '3', '7', '9'};
+
+        public void Run()
+        {            
+            var primes = new List<long>{2,3,5};
+            for(long i = 7; i <= long.MaxValue; i+=2)
+            {
+                if(validPrimes.Contains(i.ToString().Last()))
+                {
+                    var validCheck = true;
+                    var half = i / 2;
+                    foreach(var prime in primes)
+                    {
+                        if(prime > half)
+                        {
+                            break;
+                        }
+                        if(i % prime == 0)
+                        {
+                            validCheck = false;
+                            break;
+                        }
+                    }
+                    if(validCheck)
+                    {
+                        primes.Add(i);
+                        Console.WriteLine(i);
                     }
                 }
             }
